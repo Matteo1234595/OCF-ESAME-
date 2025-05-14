34ee7fd31ca258dcc7b746d9d9108d035bf9e733
Matteo1234595 <matteo.dicurzio02@gmail.com>
2025-05-14 17:15:37 +0200

    Add files via upload

---
+++ domande_data.py
@@ -0,0 +1,17 @@
+
+DOMANDE_COMPLETE = {
+    "Diritto del mercato finanziario": [
+        {
+            "domanda": "Qual è la funzione della CONSOB?",
+            "opzioni": [
+                "Gestire la politica monetaria",
+                "Autorizzare i fondi pensione",
+                "Vigilare sulla trasparenza e correttezza del mercato",
+                "Garantire i depositi bancari"
+            ],
+            "corretta": "C",
+            "spiegazione": "La CONSOB vigila sul mercato finanziario per garantire trasparenza e correttezza.",
+            "distrattore": "A"
+        }
+    ]
+}
